d0046b7f225531637a90a5c9c8141b7cc327acfc
Pattarapol Jamsai <6833212121@student.chula.ac.th>
2026-03-18 13:49:01 +0700

    fixed code 2nd

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,4 +1,4 @@
-
+//import NextAuth from "next-auth";
 import { AuthOptions } from "next-auth";
 import  CredentialsProvider  from "next-auth/providers/credentials";
 import userLogin from "@/libs/userLogIn";
@@ -46,3 +46,7 @@ export const authOptions:AuthOptions ={
         }
     }
 }
+/*
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };*/
