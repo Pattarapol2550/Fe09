811dd57eb0ed2012d8a8e1de82de7357e94b6d74
Pattarapol Jamsai <6833212121@student.chula.ac.th>
2026-03-18 13:54:59 +0700

    fixed code 3rd

--- src/app/booking/manage/@dashboard/page.tsx
+++ src/app/booking/manage/@dashboard/page.tsx
@@ -4,19 +4,42 @@ import getUserProfile from '@/libs/getUserProfile'
 
 export default async function DashboardPage(){
     const session = await getServerSession(authOptions)
-    if(!session || !session.user.token) return null
 
-    const profile = await getUserProfile(session.user.token)
-    var createdAt = new Date(profile.data.createdAt)
+    let profile = null
+    let createdAt = null
+
+    if(session?.user?.token){
+        try{
+            profile = await getUserProfile(session.user.token)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    if(profile){
+        createdAt = new Date(profile.data.createdAt)
+    }
 
     return(
         <main className="bg-slate-100 m-5 p-5">
-            <div className='text-2xl'>{profile.data.name}</div>
-            <table className='table-auto border-separate border-spacing-1'><tbody>
-                <tr><td> Email   </td> <td>{profile.data.email} </td></tr>
-                    <tr><td> Tel.  </td> <td>{profile.data.tel} </td></tr>
-                        <tr><td> Member Since :</td> <td>{createdAt.toString()} </td></tr>
-            </tbody></table>
+
+            {
+                profile ? (
+                    <>
+                        <div className='text-2xl'>{profile.data.name}</div>
+                        <table className='table-auto border-separate border-spacing-1'>
+                            <tbody>
+                                <tr><td>Email</td><td>{profile.data.email}</td></tr>
+                                <tr><td>Tel.</td><td>{profile.data.tel}</td></tr>
+                                <tr><td>Member Since</td><td>{createdAt?.toString()}</td></tr>
+                            </tbody>
+                        </table>
+                    </>
+                ) : (
+                    <div>Not logged in</div>
+                )
+            }
+
         </main>
     )
 }
